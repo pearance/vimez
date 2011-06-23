60d6ba860fb4bc563f283ea82d3402e14eefa0e1
Fontaine Cook <fontaine.cook@pearance.com>
2011-06-23 02:27:39 +0000

    Revised install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
-git clone git://github.com/vimez/vimez.git .vim
-cd .vim
 
+
+# Change directory to user $HOME
+cd $HOME
+
+# Clean up any remnants of an existing Vim install
+for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
+
+# Clone VimEz
+git clone git://github.com/vimez/vimez.git .vim
+
+# Link to vimrc and gvimrc config files
+ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/gvimrc ~/.gvimrc
+
+# Clone Vundle
+cd .vim
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -u initrc +BundleInstall +q
