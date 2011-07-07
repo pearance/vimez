617a80dae9b0905c4f532718cce98fb64f023344
Fontaine Cook <fontaine.cook@pearance.com>
2011-07-07 23:16:20 +0000

    Updating install.sh to include compilation of Command-T's C extension

--- install.sh
+++ install.sh
@@ -1,26 +1,44 @@
-
-
 # Change directory to user $HOME
 cd $HOME
+
+
 
 # Clean up any remnants of an existing Vim install
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv -f $i $i.vimez.bak; done
 rm -r .vim
 
+
+
 # Clone VimEz
 git clone git://github.com/VimEz/VimEz.git .vim
+
+
 
 # Link to vimrc and gvimrc config files
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
 
+
+
 # Clone Vundle
-cd ~/.vim
+cd ~/.vim/
 git clone http://github.com/VimEz/vundle.git ~/.vim/bundle/vundle
 vim -u initrc +BundleInstall +q
 
+
+
+# Install Command-T C extension
+rvm use system
+cd ~/.vim/bundle/Command-T/ruby/command-t/ 
+ruby extconf.rb
+make
+cd ~/.vim/
+
+
 # Clean up
-rm ~/install.sh
+#rm ~/install.sh
+
+
 
 # TODO: insert 'stty -ixon' into .profile if it exist to disable Ctrl-s and Ctrl-q
 # in terminal.
