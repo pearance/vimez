f651955b22e29293fca1023146651e5be12f1a7b
Fontaine Cook <fontaine.cook@pearance.com>
2011-07-12 00:18:06 +0000

    Updated .install.sh

--- .install.sh
+++ .install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Backup any remnants of an existing Vim install
+# Backup any remnants of an existing Vim install.
 for i in ~/.vim ~/.vimrc ~/.gvimrc
   do [ -e $i ] && mv -f $i $i.vimez.bak
 done
 rm -f ~/.vimrc
 rm -f ~/.gvimrc
-echo -e "\nBackedup existing Vim install successfully\n"
+echo -e "\nBackedup existing Vim install successfully!\n"
 
 
 
@@ -16,30 +16,32 @@ echo -e "\nCloned VimEz successfully!\n"
 
 
 
-# Link to vimrc and gvimrc config files
+# Link to vimrc and gvimrc configuration files.
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/gvimrc ~/.gvimrc
 echo -e "\nLinked to configuration files successfully!\n"
 
 
 
-# Clone Vundle and install plugin/bundles
+# Clone Vundle and install plugin/bundles.
 git clone http://github.com/VimEz/Vundle.git ~/.vim/bundle/Vundle
 vim -u ~/.vim/initrc +BundleInstall +q
 echo -e "\nPlugin bundles installed successfully!\n"
 
 
 
-# Install Command-T C extension
+# Install Command-T C extension.
 echo -e "\n"
 rvm use system
 cd ~/.vim/bundle/Command-T/ruby/command-t/ 
 ruby extconf.rb
 make
 cd
-echo -e "\nCompiled Command-T C extension successfully\n"
+echo -e "\nCompiled Command-T C extension successfully!\n"
 
-# Create local Directory
+
+
+# Create local directory.
 mkdir -p ~/.vim/local/sessions/
 mkdir -p ~/.vim/local/spell/
 mkdir -p ~/.vim/local/tmp/backups/
@@ -48,10 +50,15 @@ mkdir -p ~/.vim/local/tmp/undos/
 
 
 
-# Clean up
-rm ~/.install.sh
+# Create spell file.
+touch ~/.vim/local/spell/en.utf-8.add
+
+
+
+# Clean up.
+rm ~/install.sh
 rm ~/.vim/install.sh
-echo -e "\nCleaned up successfully\n"
+echo -e "\nCleaned up successfully!\n"
 
 
 # TODO: insert 'stty -ixon' into .profile if it exist to disable Ctrl-s and Ctrl-q
