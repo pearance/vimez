da84639841d07b3691f29236455966dc7b8f536f
Fontaine Cook <fontaine.cook@pearance.com>
2011-08-08 07:44:55 -0400

    Redirect stdout/stderr to tmp/vimez.install.log & format output

--- .aux/install.sh
+++ .aux/install.sh
@@ -20,10 +20,10 @@
 
 
 
-# Color codes.
+# Declare variables.
 NO="\033[0;39m"
 BD="\033[1;39m"
-UN="\033[4;39m"
+RE="\033[4;39m"
 RE="\033[7;39m"
 R="\033[31m"
 G="\033[32m"
@@ -32,103 +32,120 @@ B="\033[34m"
 M="\033[35m"
 C="\033[36m"
 W="\033[37m"
+
+DLY=1
 #------------------------------------------------------------------------------
 
 
 
 # Establish "Grand" Title.
 clear
-/bin/echo -e $BD$M
-/bin/echo "*************************************"
-/bin/echo "*           VimEz Install           *"
-/bin/echo "*************************************"
-/bin/echo -e $BD$B
+/bin/echo -e $BD$RE$M
+/bin/echo -e "                                       "
+/bin/echo -e "             VimEz Install             "
+/bin/echo -e "                                       "
+/bin/echo -e $NO
 #------------------------------------------------------------------------------
 
 
 
 # Backup any remnants of an existing Vim install.
+/bin/echo -e $BD$M
+/bin/echo -en "* Backing up existing Vim install... "
 cd ~
+/bin/mkdir -p ~/tmp
+/bin/rm ~/tmp/vimez.install.log
 /bin/rm -fr ~/backup/vimez/
 /bin/mkdir -p ~/backup/vimez/
 for i in .vimrc .gvimrc .vim
   do [ -e $i ] && /bin/mv -f $i backup/vimez/$i
 done
-/bin/echo -e $BD$RE$M
-/bin/echo -e "\nBackedup existing Vim install successfully!\n"
-/bin/echo -e $NO$B
+#printf "%-0s"
+sleep $DLY
+/bin/echo -e $BD$G"ok"$NO
 #------------------------------------------------------------------------------
 
 
 
 # Clone VimEz
-git clone git://github.com/VimEz/VimEz.git ~/.vim
-/bin/echo -e $BD$UN$M
-/bin/echo -e "\nCloned VimEz successfully!\n"
-/bin/echo -e $NO$B
+/bin/echo -e $BD$M
+/bin/echo -en "* Cloning VimEz...                   "
+git clone git://github.com/VimEz/VimEz.git ~/.vim >>~/tmp/vimez.install.log 2>&1
+#printf "%-19s"
+sleep $DLY
+/bin/echo -e $BD$G"ok"$NO
 #------------------------------------------------------------------------------
 
 
 
 # Link to vimrc and gvimrc configuration files.
+/bin/echo -e $BD$M
+/bin/echo -en "* Linking to configuration files...  "
 cd ~
 /bin/rm -f ~/.vimrc
 /bin/rm -f ~/.gvimrc
 /bin/ln -s .vim/vimrc .vimrc
 /bin/ln -s .vim/gvimrc .gvimrc
-/bin/echo -e $BD$UN$M
-/bin/echo -e "\nLinked to configuration files successfully!\n"
-/bin/echo -e $NO$B
+#printf "%-2s"
+sleep $DLY
+/bin/echo -e $BD$G"ok"$NO
 #------------------------------------------------------------------------------
 
 
 
-# Clone Vundle and install plugin/bundles.
-git clone http://github.com/VimEz/Vundle.git ~/.vim/bundle/Vundle
-vim -u ~/.vim/initrc +BundleInstall +q
-/bin/echo -e $BD$UN$M
-/bin/echo -e "\nPlugin bundles installed successfully!\n"
-/bin/echo -e $NO$B
-#------------------------------------------------------------------------------
-
-
-
-# Install Command-T C extension.
-/bin/echo -e "\n"
-rvm use system
-/bin/echo -e "\n"
-cd ~/.vim/bundle/Command-T/ruby/command-t/
-ruby extconf.rb
-make
-cd
-/bin/echo -e $BD$UN$M
-/bin/echo -e "\nCompiled Command-T C extension successfully!\n"
-/bin/echo -e $NO$B
-#------------------------------------------------------------------------------
-
-
-
-# Create local directories.
+# Create .vim.local structure.
+/bin/echo -e $BD$M
+/bin/echo -en "* Generating .vim.local structure... "
 /bin/mkdir -p ~/.vim.local/sessions/
 /bin/mkdir -p ~/.vim.local/spell/
 /bin/mkdir -p ~/.vim.local/tmp/backups/
 /bin/mkdir -p ~/.vim.local/tmp/swaps/
 /bin/mkdir -p ~/.vim.local/tmp/undos/
-/bin/echo -e $BD$UN$M
-/bin/echo -e "\nLocal directories created successfully!\n"
-/bin/echo -e $NO$B
+#printf "%${COL}s"
+sleep $DLY
+/bin/echo -e $BD$G"ok"$NO
 #------------------------------------------------------------------------------
 
 
 
-# Populate local directories.
+# Populate .vim.local structure.
+/bin/echo -e $BD$M
+/bin/echo -en "* Populating .vim.local structure... "
 touch ~/.vim.local/spell/en.utf-8.add
 /bin/cp -f ~/.vim/.aux/initrc.local.example ~/.vim.local/
 /bin/cp -f ~/.vim/.aux/vimrc.local.example ~/.vim.local/
 /bin/cp -f ~/.vim/.aux/gvimrc.local.example ~/.vim.local/
-/bin/echo -e $BD$UN$M
-/bin/echo -e $BD$UN$M"\nLocal directories populated successfully!\n"
-/bin/echo -e $NO$B
+#printf "%${COL}s"
+sleep $DLY
+/bin/echo -e $BD$G"ok"$NO
+#------------------------------------------------------------------------------
+
+
+
+# Clone Vundle and install plugin/bundles.
+/bin/echo -e $BD$M
+/bin/echo -en "* Installing plugin bundles...       "
+git clone http://github.com/VimEz/Vundle.git ~/.vim/bundle/Vundle >>~/tmp/vimez.install.log 2>&1
+vim -u ~/.vim/initrc +BundleInstall "+let g:session_directory = '~/.vim.local/sessions/'" +q >>~/tmp/vimez.install.log 2>&1
+/bin/rm -r ~/.vim/sessions
+#printf "%${COL}s"
+sleep $DLY
+/bin/echo -e $BD$G"ok"$NO
+#------------------------------------------------------------------------------
+
+
+
+# Install Command-T C extension.
+/bin/echo -e $BD$M
+/bin/echo -en "* Compiling Command-T extensions...  "
+rvm use system >>~/tmp/vimez.install.log 2>&1
+cd ~/.vim/bundle/Command-T/ruby/command-t/
+ruby extconf.rb >>~/tmp/vimez.install.log 2>&1
+make >>~/tmp/vimez.install.log 2>&1
+cd ~
+#printf "%${COL}s"
+sleep $DLY
+/bin/echo -e $BD$G"ok"$NO
 #------------------------------------------------------------------------------
 
 
@@ -137,13 +154,23 @@ touch ~/.vim.local/spell/en.utf-8.add
 #/bin/mv ~/.vim/bundle/ConqueShell/doc/tags ~/.vim.local/tmp/
 #cd ~/.vim/bundle/ConqueShell/; git checkout 2.0; cd ~/.vim
 #/bin/mv ~/.vim.local/tmp/tags ~/.vim/bundle/ConqueShell/doc/
+#printf "%${COL}s"
+#/bin/echo -en $BD$G "ok"$NO
 #------------------------------------------------------------------------------
 
 
 
 # Clean up.
-/bin/rm ~/install.sh
-/bin/echo -e $BD$UN$M
-/bin/echo -e "\nCleaned up successfully!\n"
-/bin/echo -e $NO$B
+/bin/echo -e $BD$M
+/bin/echo -en "* Cleaning up...                     "
+/bin/rm ~/install.sh >>~/tmp/vimez.install.log 2>&1
+#printf "%${COL}s"
+sleep $DLY
+/bin/echo -e $BD$G"ok"$NO
 #------------------------------------------------------------------------------
+
+
+
+# Outro
+/bin/echo -e $BD$C"\nInstallation Complete\n\n"
+/bin/echo -e "                    Happy vimming!"
