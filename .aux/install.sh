1b1213d2c6522e0959c705c6d845247fb6696b69
Fontaine Cook <fontaine cook@pearance.com>
2013-05-05 06:50:36 -0400

    Move colorshceme into root of .vim

--- .aux/install.sh
+++ .aux/install.sh
@@ -138,7 +138,6 @@ sleep $DLY
 /bin/echo -en "6 Installing plugin bundles...       "
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle >>/tmp/vimez.install.log 2>&1
-git clone https://github.com/vimez/vim-themes.git ~/.vim/bundle/vim-themes >>/tmp/vimez.install.log 2>&1
 vim "+let g:session_directory = '~/.vim.local/tmp/sessions/'" +BundleInstall +qall >>/tmp/vimez.install.log 2>&1
 # /bin/rm -r ~/.vim/sessions
 cd ~
