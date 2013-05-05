d392c145467aaf51775172e9e3717a26e0c832e2
Fontaine Cook <fontaine cook@pearance.com>
2013-05-05 06:23:55 -0400

    Troubleshoot

--- .aux/install.sh
+++ .aux/install.sh
@@ -138,8 +138,10 @@ sleep $DLY
 /bin/echo -en "6 Installing plugin bundles...       "
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle >>/tmp/vimez.install.log 2>&1
-vim -U ~/.vim/initrc "+let g:session_directory = '~/.vim.local/tmp/sessions/'" "+let g:session_autosave = 'yes'" +BundleInstall! +qall >>/tmp/vimez.install.log 2>&1
+git clone https://github.com/vimez/vim-themes.git ~/.vim/bundle/vim-themes >>/tmp/vimez.install.log 2>&1
+vim -U ~/.vim/initrc "+let g:session_directory = '~/.vim.local/tmp/sessions/'" "+let g:session_autosave = 'yes'" +BundleInstall +qall >>/tmp/vimez.install.log 2>&1
 # /bin/rm -r ~/.vim/sessions
+cd ~
 /bin/ln -s .vim/vimrc .vimrc
 
 sleep $DLY
