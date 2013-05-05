5c21678b1778463276e8d1e4da206d36b447214f
Fontaine Cook <fontaine cook@pearance.com>
2013-05-05 18:35:45 -0400

    Misc update

--- .aux/install.sh
+++ .aux/install.sh
@@ -44,7 +44,7 @@ clear
 /bin/echo -e "              Vimez Install                 "
 /bin/echo -e "                                            "
 /bin/echo -e $NO
-/bin/echo -e $BD$G"Note: step 6 can take up to three minutes"$NO
+/bin/echo -e $BD$G"Note: step 5 can take up to three minutes"$NO
 /bin/echo -e $BD$G"to complete. Please be patient."$NO
 
 #------------------------------------------------------------------------------
